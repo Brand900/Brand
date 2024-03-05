468f169086f68d83a1ab49fa6c46d5088329c3b2
Brand900 <162292228+Brand900@users.noreply.github.com>
2024-03-05 13:09:52 +0500

    Update Brand.py

--- Brand.py
+++ Brand.py
@@ -1,1 +1,7 @@
 
+import os,platform
+os.system('git pull')
+# exit(' Wait Tool On updating ')
+Brand=platform.architecture()[0]
+if Brand=="32bit":__import__("Brand32")
+elif Brand=="64bit":__import__("Brand64")
